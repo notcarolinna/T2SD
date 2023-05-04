d7bef60e16660249fb3b21b1ad67ac1709fd0897
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-04 10:18:04 -0300

    Update timer.v

--- timer.v
+++ timer.v
@@ -1,6 +1,11 @@
 module timer 
 (
-  input 
+  input rst, //reset do módulo que é ativo alto (‘1’)
+  input clk, //clock rápido de 10 Hz;
+  input t_en, //enable do módulo que indica quando produzir o dado de saída
+  
+  output t_valid, //indica que o valor colocado no sinal de saída é válido
+  output [15:0]t_out //sinal que contém o valor positivo gerado,  caso o valor produzido estoure essa representação numérica, você deve apresentar sempre os 16 bits menos significativos
 );
 
 endmodule
