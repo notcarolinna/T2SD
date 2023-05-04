5957b19c28d649d4e3547e2a6a1fb234af51a181
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-04 10:12:26 -0300

    Update fibonacci.v

--- fibonacci.v
+++ fibonacci.v
@@ -1,6 +1,11 @@
 module fibonacci 
 (
-  // Declarar os pinos de IO
+  input reset, //reset do módulo que é ativo alto (‘1’)
+  input clk, // clock
+  input f_en, //enable do módulo que indica quando produzir o dado de saída
+  
+  output f_valid, // verifica se o sinal de saída é valido
+  output [15:0] f_out // valor atual da sequencia de fibonacci
 );
 
 endmodule
