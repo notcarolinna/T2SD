764c154bd17248507b76e510b0eeebab5489eddb
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-04 10:23:24 -0300

    Update wrapper.v

--- wrapper.v
+++ wrapper.v
@@ -3,4 +3,6 @@ module wrapper
   // Declarar os pinos de IO
 );
 
+  // se a frequencia de leiura for mais q a de escrita o buffer vai enchar e qnd isso acontecer o buffer tem q mandar um sinal pro timer parar de produzir as infos e isso vai criar uma saída incorreta
+  
 endmodule
