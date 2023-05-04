e6434d32ddfedaef990bcf3fc55ca692adcb9acd
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-04 10:22:00 -0300

    Update dcm.v

--- dcm.v
+++ dcm.v
@@ -1,6 +1,13 @@
 module dcm 
 (
-  // Declarar os pinos I/O
+  input rst, //reset do módulo que é ativo alto (‘1’);
+  input clk, // clock de referência deste módulo síncrono que opera a 100 MHz
+  input update, //
+  input [2:0]prog_in,
+  
+  output clk_1,
+  output clk_2,
+  output [2:0]prog_out
 );
 
 endmodule
