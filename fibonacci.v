15c303a3f5a9aa723200405173c89bd310bea841
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-17 16:51:31 -0300

    Update fibonacci.v

--- fibonacci.v
+++ fibonacci.v
@@ -8,32 +8,31 @@ module fibonacci
   output [15:0] f_out // valor atual da sequencia de fibonacci
 );
   
-  reg [15:0] t1, t2, prox;
-  
-    
+  reg [15:0] t1, t2;
+   
  
    // Cálculo Fibonacci
   always @(posedge clk or posedge rst)
     begin
       if(rst == 1) 
       begin // se o reset for ativado
-        t1 <= 16'd0; // retorna o valor de t1 para 0
-        t2 <= 16'd1; // e retorna o valor de t2 para 1
+        t1 <= 16'd0; // t1 = posição anterior
+        t2 <= 16'd1; // t2 = posição atual
+        f_valid <= 1'b0;
       end
       else 
       begin // senão
         if(f_en == 1) begin // se o enable estiver ativo
-        f_valid <= 1'b1;  // o f_valid define que a saída é válida
-        prox <= t1 + t2; // o próximo número de Fibbonacci e a soma dos dois antecessores
-        t1 <= t2;
-        t2 <= prox;
+          f_valid <= 1'b1;  // o f_valid define que a saída é válida
+          t1 <= t2; // o anterior recebe o atual
+          t2 <= t2 + t1; // o atual recebe ele mais o anterior
         end
-      
-      
         else begin
           f_valid <= 1'b0; // se o enable não for ativado, a saída é considerada inválida
         end
       end
     end
+
+assign f_out = t1;
 
 endmodule
